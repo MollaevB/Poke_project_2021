a9ceb4b6823d873c86c1e477bf9d8c9372bb893e
TheoOui <73181894+TheoOui@users.noreply.github.com>
2021-01-12 12:45:22 +0100

    Update cake.php

--- bin/cake.php
+++ bin/cake.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php -q
 <?php
-// Check platforms requirements
+// Check platform requirements
 require dirname(__DIR__) . '/config/requirements.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
 
